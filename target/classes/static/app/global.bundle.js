35ce1d82aa0c35f3cacfb929b205c90300ee51bf
weinda <davidweinberger@gmx.at>
2019-10-08 18:55:02 +0200

    08102019 added cart entity 2

--- target/classes/static/app/global.bundle.js
+++ target/classes/static/app/global.bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a84609a499599f87b350";
+/******/ 	var hotCurrentHash = "a7a7e90ef368b52a0ce2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
