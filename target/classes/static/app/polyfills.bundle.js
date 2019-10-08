373a6240c6adba0625c224cd9fa11e2811f48089
weinda <davidweinberger@gmx.at>
2019-10-08 22:59:38 +0200

    08102019 create tabletPlan

--- target/classes/static/app/polyfills.bundle.js
+++ target/classes/static/app/polyfills.bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b5160768530b692eff2a";
+/******/ 	var hotCurrentHash = "b909273a775e169f4a8e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
